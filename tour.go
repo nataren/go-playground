7b2caca802a3d8881505b1a9f93a425bce07f4b5
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 14:50:55 -0700

    Fibonnacci closures!

--- tour.go
+++ tour.go
@@ -2,20 +2,27 @@ package main
 
 import "fmt"
 
-func adder() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+func fib(i int) int {
+	if i == 0 || i == 1 {
+		return 1
+	} else {
+		return fib(i - 1) + fib(i - 2)
+	}
+}
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	fibIter := 0
+	return func() int {
+		fibIter += 1
+		return fib(fibIter)
 	}
 }
 
 func main() {
-	pos, neg := adder(), adder()
-	for i := 0; i < 10; i++ {
-		fmt.Println(
-			pos(i),
-			neg(-2 * i),
-		)
-	}
+    f := fibonacci()
+    for i := 0; i < 10; i++ {
+        fmt.Println(f())
+    }
 }
