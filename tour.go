956b2904c20086a0b510a8aa0e72fbcd14e7276f
César López-Natarén <cesarn@mindtouch.com>
2014-10-18 21:53:59 -0700

    multiple results

--- tour.go
+++ tour.go
@@ -1,13 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
