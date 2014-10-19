8fe94a10982dc316455d2e843fe99cc28764e856
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 12:44:01 -0700

    Making slices

--- tour.go
+++ tour.go
@@ -3,13 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	p := []int { 2, 3, 5, 7, 11, 13 }
-	fmt.Println("p == ", p)
-	fmt.Println("p[1:4] ==", p[1:4])
 
-	// missing low index implies 0
-	fmt.Println("p[:3] ==", p[:3])
+	// a
+	a := make([]int, 5)
+	printSlice("a", a)
 
-	// missing high index implies len(s)
-	fmt.Println("p[4:] ==", p[4:])
+	// b
+	b := make([]int, 0, 5)
+	printSlice("b", b)
+
+	// c
+	c := b[:2]
+	printSlice("c", c)
+
+	// d
+	d := c[2:5]
+	printSlice("d", d)
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
