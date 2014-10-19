ad9f77a2667c502de585d5cf0c67a0fcdef97509
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 12:45:35 -0700

    nil slice

--- tour.go
+++ tour.go
@@ -3,25 +3,9 @@ package main
 import "fmt"
 
 func main() {
-
-	// a
-	a := make([]int, 5)
-	printSlice("a", a)
-
-	// b
-	b := make([]int, 0, 5)
-	printSlice("b", b)
-
-	// c
-	c := b[:2]
-	printSlice("c", c)
-
-	// d
-	d := c[2:5]
-	printSlice("d", d)
-}
-
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n",
-		s, len(x), cap(x), x)
+	var z []int
+	fmt.Println(z, len(z), cap(z))
+	if z == nil {
+		fmt.Println("nil!")
+	}
 }
