653a516f50ff97fcf88047bbd81eb586ca65b1d1
César López-Natarén <cesarn@mindtouch.com>
2014-10-18 21:30:27 -0700

    Packages

--- tour.go
+++ tour.go
@@ -2,9 +2,11 @@ package main
 
 import (
     "fmt"
+    "math/rand"
     "time"
 )
 
 func main() {
-    fmt.Println("The time is", time.Now())
+	rand.Seed(time.Now().Unix())
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
