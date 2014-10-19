8f176903e53f4ee9d36af4acb5d69f34cc700890
César López-Natarén <cesarn@mindtouch.com>
2014-10-18 21:41:00 -0700

    exported names

--- tour.go
+++ tour.go
@@ -1,9 +1,10 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Now you have %g problems.",
-		math.Nextafter(2, 3))
+	fmt.Println(math.Pi)
 }
