b4f1a5666e934bbd7cbdbcf9e86c7bbdaaca307f
César López-Natarén <cesarn@mindtouch.com>
2014-10-18 22:07:10 -0700

    Basic type

--- tour.go
+++ tour.go
@@ -1,10 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+var (
+	ToBe bool     = false
+	MaxInt uint64 = 1<<64 - 1
+	z complex128  = cmplx.Sqrt(-5 + 12i)
+)
 
 func main() {
-	var i, j int = 1, 2
-	k := 3
-	c, python, java := true, false, "no!"
-	fmt.Println(i, j, k, c, python, java)
+	const f = "%T(%v)\n"
+	fmt.Printf(f, ToBe, ToBe)
+	fmt.Printf(f, MaxInt, MaxInt)
+	fmt.Printf(f, z, z)
 }
