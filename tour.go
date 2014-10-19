1c2c904d847b6a0c71efd5f4dc9f913e961e0e0c
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 12:28:44 -0700

    Slicing slices

--- tour.go
+++ tour.go
@@ -5,8 +5,11 @@ import "fmt"
 func main() {
 	p := []int { 2, 3, 5, 7, 11, 13 }
 	fmt.Println("p == ", p)
+	fmt.Println("p[1:4] ==", p[1:4])
 
-	for i := 0; i < len(p); i++ {
-		fmt.Printf("p[%d] == %d\n", i, p[i])
-	}
+	// missing low index implies 0
+	fmt.Println("p[:3] ==", p[:3])
+
+	// missing high index implies len(s)
+	fmt.Println("p[4:] ==", p[4:])
 }
