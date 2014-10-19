659f852ca5e0aa46b41e810282f7bbaa03997302
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 13:39:53 -0700

    Maps exercise

--- tour.go
+++ tour.go
@@ -1,18 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "code.google.com/p/go-tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+    words := strings.Fields(s)
+    counts := make(map[string]int)
+	for _, word := range words {
+		count, ok := counts[word]
+		if ok {
+			counts[word] = count + 1
+		} else {
+			counts[word] = 1
+		}
+	}
+	return counts
+}
 
 func main() {
-	m := make(map[string]int)
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+    wc.Test(WordCount)
 }
