be2289926ebe0db49dbae29a4fb3bbbc0f950568
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 11:57:03 -0700

    Use Newton's method to compute the square root

--- tour.go
+++ tour.go
@@ -5,17 +5,43 @@ import (
 	"math"
 )
 
-func pow(x, n, lim float64) float64 {
-	if v := math.Pow(x, n); v < lim {
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim)
+func MySqrt(x float64) float64 {
+	return SqrtIter(1.0, x)
+}
+
+func SqrtIter(guess, x float64) float64 {
+	for GoodEnough(guess, x) != true {
+		guess = Improve(guess, x)
 	}
-	return lim
+	return guess
+}
+
+func GoodEnough(guess, x float64) bool {
+	return math.Abs(math.Pow(guess, 2) - x) < 0.001
+}
+
+func Improve(guess, x float64) float64 {
+	return Average(guess, x / guess)
+}
+
+func Average(x, y float64) float64 {
+	return (x + y) / 2
 }
 
 func main() {
-	fmt.Println(
-		pow(3, 2, 10),
-		pow(3, 3, 20))
+	fmt.Println("MySqrt: ", MySqrt(1))
+	fmt.Println("math.Sqrt: ", math.Sqrt(1))
+
+	fmt.Println("MySqrt: ", MySqrt(2))
+	fmt.Println("math.Sqrt: ", math.Sqrt(2))
+
+	fmt.Println("MySqrt: ", MySqrt(3))
+	fmt.Println("math.Sqrt: ", math.Sqrt(3))
+
+	fmt.Println("MySqrt: ", MySqrt(4))
+	fmt.Println("math.Sqrt: ", math.Sqrt(4))
+
+	fmt.Println("MySqrt: ", MySqrt(5))
+	fmt.Println("math.Sqrt: ", math.Sqrt(5))
+
 }
