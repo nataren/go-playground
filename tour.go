a88c99912740e427da6550ce6ee48c11eb57fad5
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 12:25:43 -0700

    Slices

--- tour.go
+++ tour.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
+	p := []int { 2, 3, 5, 7, 11, 13 }
+	fmt.Println("p == ", p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Printf("p[%d] == %d\n", i, p[i])
+	}
 }
