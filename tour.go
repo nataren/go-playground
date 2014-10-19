5b53b441941bf8408ed1fcca0ac6e92ac8f0b346
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 13:41:58 -0700

    function literals

--- tour.go
+++ tour.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-    "code.google.com/p/go-tour/wc"
-	"strings"
+	"fmt"
+	"math"
 )
 
-func WordCount(s string) map[string]int {
-    words := strings.Fields(s)
-    counts := make(map[string]int)
-	for _, word := range words {
-		count, ok := counts[word]
-		if ok {
-			counts[word] = count + 1
-		} else {
-			counts[word] = 1
-		}
-	}
-	return counts
-}
-
 func main() {
-    wc.Test(WordCount)
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x * x + y * y)
+	}
+	fmt.Println(hypot(3, 4))
 }
