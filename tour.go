d16898291a3619acabd3e863d8c7bb04f60b8182
César López-Natarén <cesarn@mindtouch.com>
2014-10-18 22:11:40 -0700

    type conversations

--- tour.go
+++ tour.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
-)
-
-var (
-	ToBe bool     = false
-	MaxInt uint64 = 1<<64 - 1
-	z complex128  = cmplx.Sqrt(-5 + 12i)
+	"math"
 )
 
 func main() {
-	const f = "%T(%v)\n"
-	fmt.Printf(f, ToBe, ToBe)
-	fmt.Printf(f, MaxInt, MaxInt)
-	fmt.Printf(f, z, z)
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x * x + y * y))
+	var z int = int(f)
+	fmt.Println(x, y, z)
 }
