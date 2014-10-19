fd7ddd326912d382a6d3dd91a18a67c1da61f951
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 15:04:34 -0700

    switch

--- tour.go
+++ tour.go
@@ -1,28 +1,22 @@
 package main
 
-import "fmt"
-
-func fib(i int) int {
-	if i == 0 || i == 1 {
-		return 1
-	} else {
-		return fib(i - 1) + fib(i - 2)
-	}
-}
-
-// fibonacci is a function that returns
-// a function that returns an int.
-func fibonacci() func() int {
-	fibIter := 0
-	return func() int {
-		fibIter += 1
-		return fib(fibIter)
-	}
-}
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-    f := fibonacci()
-    for i := 0; i < 10; i++ {
-        fmt.Println(f())
-    }
+	fmt.Print("Go runs on ")
+
+	// Cases
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+
+	case "linux":
+		fmt.Println("Linux.")
+
+	default:
+		fmt.Printf("%s.", "")
+	}
 }
