b1622e046c2382b91fb3cf198ebdeb512f6441e4
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 22:22:18 -0700

    channels!

--- tour.go
+++ tour.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(1)
-		fmt.Println(s)
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
 	}
+	c <- sum
 }
 
 func main() {
-	go say("world")
-	say("hello")
+	a := []int { 7, 2, 8, -9, 4, 0 }
+	c := make(chan int)
+	go sum(a[len(a) / 2:], c)
+	go sum(a[:len(a) / 2], c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, x + y)
 }
