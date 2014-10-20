0e387f041f8416d0a3b3a0174f2bdcc180a72f88
César López-Natarén <cesarn@mindtouch.com>
2014-10-19 22:34:16 -0700

    closing the channel from the sender

--- tour.go
+++ tour.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 )
 
-func sum(a []int, c chan int) {
-	sum := 0
-	for _, v := range a {
-		sum += v
-	}
-	c <- sum
+func fibonacci (n int, c chan int) {
+    x, y := 0, 1
+    for i := 0; i < n; i++ {
+        c <- x
+        x, y = y, x + y
+    }
+    close (c)
 }
 
-func main() {
-	a := []int { 7, 2, 8, -9, 4, 0 }
-	c := make(chan int)
-	go sum(a[len(a) / 2:], c)
-	go sum(a[:len(a) / 2], c)
-	x, y := <-c, <-c
-	fmt.Println(x, y, x + y)
+func main () {
+    c := make (chan int, 10)
+    go fibonacci (cap (c), c)
+    for i := range c {
+        fmt.Println (i)
+    }
 }
